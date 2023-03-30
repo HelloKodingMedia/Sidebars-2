13ac91aeb9f9a7320e4917aaf120b4155a2076c7
Hello Koding Media <128211871+HelloKodingMedia@users.noreply.github.com>
2023-03-30 22:17:08 +0700

    Add files via upload

---
+++ Sidebars #2/script.js
@@ -0,0 +1,11 @@
+// script.js
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let srcBtn = document.querySelector(".bx-search");
+
+btn.onclick = function () {
+  sidebar.classList.toggle("active");
+};
+srcBtn.onclick = function () {
+  sidebar.classList.toggle("active");
+};
